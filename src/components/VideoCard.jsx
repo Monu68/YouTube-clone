d3021fc5d061e8966d6d49f7c80ad935cf5370c1
Manish Kumar Monu <92303219+Monu68@users.noreply.github.com>
2025-03-25 23:24:58 +0530

    Videocards

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
 const VideoCard = ({info}) => {
-    console.log(info);  
+    if (!info) return <div>Loading...</div>;
+    console.log(info); 
+    const {snippet} = info;
+    const {channelTitle, title, thumbnails} = snippet; 
   return (
-      <div>VideoCard</div>
+      <div className='p-2 m-2 w-56 shadow-lg'>
+        <img className='rounded-lg ' alt='thumbnail'
+        src= {thumbnails.medium.url} />
+        <ul>
+            <li className='font-bold'>{title}</li>
+            <li>{channelTitle}</li>
+            {/* <li>{statistics.viewCount}</li> */}
+        </ul>
+      </div>
+
   )
 }
 
