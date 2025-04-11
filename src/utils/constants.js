289bb0372bae221c160b528d69bda4ec49883259
Manish Kumar Monu <92303219+Monu68@users.noreply.github.com>
2025-04-11 21:24:49 +0530

    fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,4 +5,4 @@ export const LIVE_CHAT_COUNT = 30;
 export const YOUTUBE_VIDEOS_API = 
 `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&regionCode=IN&maxResults=20&key=${GOOGLE_API_KEY}`;
 
-export const YOUTUBE_SEARCH_API = 'http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=';
+export const YOUTUBE_SEARCH_API = 'https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=';
